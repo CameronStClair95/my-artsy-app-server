e5ea3a2453ee96d9d930a7ab0952f2522ebd319a
Maksym Kopychanskyi <kopy.chan.max@gmail.com>
2023-03-25 15:34:27 +0000

    working with the user routes

--- routes/post.routes.js
+++ routes/post.routes.js
@@ -10,6 +10,16 @@ const { isAuthenticated } = require("../middleware/jwt.middleware.js");
 
 const {isLoggedIn, isLoggedOut, isAdmin} = require("../middleware/route-guard")
 
+// POST "/api/upload" => Route that receives the image, sends it to Cloudinary via the fileUploader and returns the image URL
+router.post("/upload", fileUploader.single("imageUrl"), (req, res, next) => {
+    if (!req.file) {
+      next(new Error("No file uploaded!"));
+      return;
+    }
+    
+    res.json({ fileUrl: req.file.path });
+  });
+
 // POST route for creating a new post of type "artpost"
 router.post("/artpost", (req, res, next) => {
     const { artist, title, description, medium, year, dimensions, art_image, author } = req.body;
@@ -20,7 +30,7 @@ router.post("/artpost", (req, res, next) => {
         return;
     }
     // Create new Artpost object and save to database
-    Artpost.create({ artist, title, description, medium, year, dimensions, art_image, /* author:req.session.currentUser._id  */})
+    Artpost.create({ artist, title, description, medium, year, dimensions, art_image})
         .then(response => {
             console.log("Success: Artpost created");
             res.json(response);
@@ -31,17 +41,6 @@ router.post("/artpost", (req, res, next) => {
         })
 });
 
-// POST "/api/upload" => Route that receives the image, sends it to Cloudinary via the fileUploader and returns the image URL
-router.post("/upload", fileUploader.single("imageUrl"), (req, res, next) => {
-    // console.log("file is: ", req.file)
-   
-    if (!req.file) {
-      next(new Error("No file uploaded!"));
-      return;
-    }
-    
-    res.json({ fileUrl: req.file.path });
-  });
 
 // POST route for creating a new post of type "post"
 router.post("/post", (req, res, next) => {
@@ -53,7 +52,7 @@ router.post("/post", (req, res, next) => {
         return;
     }
     // Create new Post object and save to database
-    Post.create({ content, post_image, place, author:req.session.currentUser._id })
+    Post.create({ content, post_image, place })
         .then(response => {
             console.log("Success: Post created");
             res.json(response);
